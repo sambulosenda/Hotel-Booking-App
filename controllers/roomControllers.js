9841511d57a2476649299a56d495bae6f6e62757
sambulo senda <sambulosenda@sambulos-MacBook-Pro.local>
2021-11-12 11:10:23 +0000

    room controller fixed and all crud operations added

--- controllers/roomControllers.js
+++ controllers/roomControllers.js
@@ -1,3 +1,4 @@
+import { info } from "autoprefixer";
 import Room from "../models/room"
 
 //Get all rooms => /api/rooms/
@@ -17,6 +18,7 @@ const getRooms = async (req, res) => {
     }
 
 }
+
 //Create new room => /api/rooms/create
 const newRoom = async (req, res) => {
     try {
@@ -33,9 +35,91 @@ const newRoom = async (req, res) => {
     }
 }
 
+//Get room details => /api/rooms/:id
+const getSingleRoom = async (req, res) => {
+    try {
+        const room = await Room.findById(req.query.id)
+
+        if(!room){
+            return res.status(404).json({
+                success: false,
+                error: "Room not found with this id"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            room
+        })
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            error: error.message
+        })
+    }
+}
+
+//Update room details => /api/rooms/:id
+const updateRoom = async (req, res) => {
+    try {
+        let room = await Room.findById(req.query.id)
+        if(!room){
+            return res.status(404).json({
+                success: false,
+                error: "Room not found with this id"
+            })
+        }
+
+        room = await Room.findByIdAndUpdate(req.query.id, req.body, {
+            new: true, 
+            runValidators: true, 
+            useFindAndModify: false
+        })
+
+        res.status(200).json({
+            success: true,
+            room
+        })
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            error: error.message
+        })
+    }
+}
+
+
+//Delete room => /api/rooms/:id
+const deleteRoom = async (req, res) => {
+    try {
+        const room = await Room.findById(req.query.id)
+        
+        if(!room){
+            return res.status(404).json({
+                success: false,
+                error: "Room not found with this id"
+            })
+        }
+
+       await room.remove()
+
+        res.status(200).json({
+            success: true, 
+            message: "Room deleted successfully"
+        })
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            error: error.message
+        })
+    }
+}
 
 
 export {
     getRooms,
     newRoom,
+    getSingleRoom,
+    updateRoom,
+    deleteRoom
 }
